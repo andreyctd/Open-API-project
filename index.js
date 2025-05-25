f2f396555064620c26f2c8444151db591b22f8c2
Andrey Gnusarev <kseniiadobrovolskaia@MacBook-Pro-Kseniia.local>
2025-05-24 23:16:05 -0400

    Open-API-project added

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+fetch(`https://api.artic.edu/api/v1/artworks`)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`Request failed`);
+        }
+        return response.json(); // Parse the response as JSON
+    })
+    .then(data => {
+        artworks = data.data;
+        console.log("Artworks:", artworks);
+    })
+    .catch (error => {
+        console.error(`An error occurred:`, error);
+    });
